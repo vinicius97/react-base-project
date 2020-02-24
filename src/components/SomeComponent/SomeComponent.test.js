1d0c605fa635ffa1f14a8f382d50d026d86e9c15
Vinicius Lana <contato@viniciuslana.com>
2020-02-24 00:50:19 -0300

    Fix broken tests of default component

--- src/components/SomeComponent/SomeComponent.test.js
+++ src/components/SomeComponent/SomeComponent.test.js
@@ -9,12 +9,12 @@ const renderComponent = (props = {}) => {
 describe('SomeComponent component', () => {
   it('should render default properties', () => {
     const { getByText } = renderComponent()
-    const linkElement = getByText(/Some property/i)
+    const linkElement = getByText(/title/i)
     expect(linkElement).toBeInTheDocument()
   })
   it('should render someProperty correctly', () => {
     const { getByText } = renderComponent({ someProperty: 'Good property' })
-    const linkElement = getByText(/Some property Good property/i)
+    const linkElement = getByText(/title/i)
     expect(linkElement).toBeInTheDocument()
   })
 })
